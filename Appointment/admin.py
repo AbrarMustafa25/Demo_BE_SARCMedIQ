d8d4338b52c2d4081a6a0abe2f2ef662ae57bfb9
Abrar Mustafa <abrarmustafa25@gmail.com>
2025-02-22 22:42:42 +0500

    admin view fields
    admin view fields

--- Appointment/admin.py
+++ Appointment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Visit
 
 # Register your models here.
+# ==========================================================================
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'date', 'reason']
+    list_filter = ['date', 'reason']
+    search_fields = ['date', 'reason']
+
+admin.site.register(Visit, VisitAdmin)
--- Patient/admin.py
+++ Patient/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Patient
 
 # Register your models here.
+# ==========================================================================
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['mr_number', 'first_name', 'last_name', 'date_of_birth']
+    list_filter = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name']
+
+admin.site.register(Patient, PatientAdmin)
