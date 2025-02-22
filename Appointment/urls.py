2491d6a8d663c2c45f05c405027dcac93821da6d
Abrar Mustafa <abrarmustafa25@gmail.com>
2025-02-22 21:31:56 +0500

    setting up urls
    setting up urls

---
+++ Appointment/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+
+class AppointmentUrls:
+    @staticmethod
+    def get_urls():
+        urls = [
+        ]
+        return urls
+ 
--- SARCMedIQ/urls.py
+++ SARCMedIQ/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from Appointment.urls import AppointmentUrls
+from Patient.urls import PatientUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('appointment/', include(AppointmentUrls.get_urls())),
+    path('patient/', include(PatientUrls.get_urls())),
 ]
